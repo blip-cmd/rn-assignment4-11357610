f36539501f21b2cd7f78f2191860adfa59e619f1
blip-cmd <rnabrown001@st.ug.edu.gh>
2024-06-19 22:48:06 +0000

    changed directories

--- JJobizz/App.js
+++ JJobizz/App.js
@@ -1,19 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoadingScreen from './src/screens/LoadingScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/HomeScreen';
+import 'react-native-gesture-handler';
+import Home from './Home';
+import Login from './Login';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Loading">
-        <Stack.Screen name="Loading" component={LoadingScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
+        <Stack.Screen name= 'Login' component={Login}/>
+        <Stack.Screen name= 'Home' component = {Home}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
